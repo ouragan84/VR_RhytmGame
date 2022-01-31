8d052467602571b98a4f1371ed70994aec9c9b5a
Edgar <edgar.baudry@gmail.com>
2022-01-31 11:33:44 -0800

    fixed offset error

--- Assets/Scripts/WallDance.cs
+++ Assets/Scripts/WallDance.cs
@@ -10,6 +10,7 @@ public class WallDance : ObstacleInterface
     new protected void Start(){
         base.Start();
         transform.localScale = generator.transform.localScale;
+        transform.localScale = generator.transform.position;
     }
 
     void Update()
